64c3f9e41cdee0d351734a17eba8a7c591227a57
henrymendez <henry.7mendezz@gmail.com>
2025-01-07 14:01:50 -0600

    Add flashes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,19 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import "./tables/members"
 import "./tables/simple"
+
+document.addEventListener('turbo:load', () => {
+  const flashMessages = document.querySelectorAll('.flash-container');
+  flashMessages.forEach(message => {
+    setTimeout(() => {
+      message.remove();
+    }, 3000);
+  });
+
+  const loadingSpinner = document.querySelector('.loading-spinner');
+  if (loadingSpinner) {
+    setTimeout(() => {
+      loadingSpinner.style.display = 'none';
+    }, 3000);
+  }
+});
