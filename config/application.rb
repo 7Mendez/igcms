1ef3fa3c7d6d96ca0fc27700cebaf5bbb28f240f
henrymendez <henry.7mendezz@gmail.com>
2025-01-10 10:27:22 -0600

    Add domain

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module Igcms
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = "es"
+    config.hosts << "jesusteama.com.mx"
   end
 end
